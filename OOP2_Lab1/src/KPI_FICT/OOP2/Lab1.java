e9a314afe95135c23fab8ba3616e5cb23aa05150
Max Donchenko <wereya2@gmail.com>
2016-01-25 22:56:26 +0200

    Changed lab1: inverted limits swapping is possible now

--- OOP2_Lab1/src/KPI_FICT/OOP2/Lab1.java
+++ OOP2_Lab1/src/KPI_FICT/OOP2/Lab1.java
@@ -6,9 +6,19 @@ import java.util.Scanner;
  * Created by Max Donchenko on 24.01.2016.
  */
 public class Lab1 {
+    public static void swap(short[] a, short[] b) {
+        /*
+        In Java, all the arguments are passed by value. So there is no such thing as a "ref".
+        However, I achieve variable swapping by wrapping values in arrays.
+        */
+        short t = a[0];
+        a[0] = b[0];
+        b[0] = t;
+    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        short a = 1, n = 4, b = 10, m = 12 , C = 2;
+        short a, n, b, m, C = 2;
         double partSum, Sum = 0.0;
 
         try {
@@ -23,7 +33,22 @@ public class Lab1 {
             m = scan.nextShort();
 
             if ((a > n) || (b > m)) {
-                throw new Exception("Sum limits are inverted");
+                if (a > n) {
+                    short[] A = {a};
+                    short[] N = {n};
+                    swap(A, N);
+                    a = A[0];
+                    n = N[0];
+                    // it's possible to delete arrays A and N here
+                }
+                if (b > m) {
+                    short[] B = {b};
+                    short[] M = {m};
+                    swap(B, M);
+                    b = B[0];
+                    m = M[0];
+                }
+                System.out.println("Inverted limits were swapped");
             }
 
             for (int i = a; i <= n; i++) {
@@ -36,9 +61,6 @@ public class Lab1 {
                         Sum += partSum;
                         System.out.println(Sum);
                     }
-                    catch (ArithmeticException e) {
-                        System.err.println("Division by zero, action was ignored");
-                    }
                     catch (Exception e2) {
                         e2.printStackTrace();
                     }
@@ -47,6 +69,7 @@ public class Lab1 {
 
             System.out.print("Sum is ");
             System.out.println(Sum);
+            
         } catch (Exception e1) {
             e1.printStackTrace();
         }
