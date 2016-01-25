6a4a6f2788e14c5f8d53f1bc5efc476431176921
Max Donchenko <wereya2@gmail.com>
2016-01-25 23:32:20 +0200

    Changed files structure: project <Labs>, package <KPI_FICT.OOP2>, one class for each lab

---
+++ OOP2_Labs/src/KPI_FICT/OOP2/Lab2_var3.java
@@ -0,0 +1,10 @@
+package KPI_FICT.OOP2;
+
+/**
+ * Created by Max Donchenko (https://github.com/goodwin64) on 25.01.2016.
+ */
+public class Lab2_var3 {
+    public static void main(String[] args) {
+
+    }
+}
