858c2687bd4b3fe564807397d7fbe7938a8a501c
Max Donchenko <wereya2@gmail.com>
2016-01-27 23:39:05 +0200

    Created empty class for Lab3; added task for Lab3.
    Amended commit

---
+++ OOP2_Labs/src/KPI_FICT/OOP2/Lab3_var3.java
@@ -0,0 +1,11 @@
+package KPI_FICT.OOP2;
+
+/**
+ * Created by Max Donchenko (https://github.com/goodwin64) on 27.01.2016.
+ */
+
+public class Lab3_var3 {
+    public static void main(String[] args) {
+
+    }
+}
