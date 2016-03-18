e494a5c7bb212bbcad19dd96bd95eab882273753
Max Donchenko <wereya2@gmail.com>
2016-03-18 17:21:20 +0200

    Lab 5: added initial implementation

--- OOP2_Labs/src/KPI_FICT/OOP2/Classes/Lab5_var3.java
+++ OOP2_Labs/src/KPI_FICT/OOP2/Classes/Lab5_var3.java
@@ -1,11 +1,35 @@
 package KPI_FICT.OOP2.Classes;
 
+import java.util.ArrayList;
+
 /**
  * Created by Max Donchenko (https://github.com/goodwin64) on 09.03.2016.
  */
 public class Lab5_var3 {
     public static void main(String[] args) {
+        /*
+        * 1) Create the empty text.
+        * 2) Output it.
+        * 3) Changing it items.
+        * 4) Output the edited text.
+        */
 
+        Text text = new Text();
+        System.out.println(text);
+
+        ArrayList<SentenceElement> sentElements = new ArrayList<>();
+
+        sentElements.add(new Word("Buenos Aires"));
+        sentElements.add(new PunctuationMark(","));
+        sentElements.add(new PunctuationMark());
+        sentElements.add(new Word("Córdoba"));
+        sentElements.add(new PunctuationMark(",-"));
+        sentElements.add(new PunctuationMark(","));
+        sentElements.add(new PunctuationMark());
+        sentElements.add(new Word("La Plata"));
+        text.value.set(0, new Sentence(sentElements));
+
+        System.out.println(text);
     }
 }
 
@@ -14,6 +38,33 @@ public class Lab5_var3 {
  * A..Z, a..z
  */
 class Word extends SentenceElement {
+    private String value;
+
+    public Word() {
+        this.value = "default";
+    }
+    public Word(String word) {
+        try {
+            setValue(word);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }
 
 /**
@@ -22,6 +73,53 @@ class Word extends SentenceElement {
  * [.!?\n] in text between sentences.
  */
 class PunctuationMark extends SentenceElement {
+    private char value;
+
+    public PunctuationMark() {
+        super();
+        setValue(" ");
+    }
+    public PunctuationMark(String punctuationMark) {
+        super(punctuationMark);
+        try {
+            setValue(punctuationMark);
+        } catch (IllegalArgumentException e) {
+            /*
+             * System.out and System.err are different console streams,
+             * so here is using the first one
+             * not to mix incorrect argument and error info.
+             */
+            System.out.printf("%s (%s)\n", e, punctuationMark);
+        }
+    }
+
+    @Override
+    public String getValue() {
+        if (super.getValue().length() > 1) {
+            return "";
+        } else {
+            return String.valueOf(this.value);
+        }
+    }
+
+    @Override
+    public void setValue(String value) throws IllegalArgumentException {
+        switch (value.length()) {
+            case 1:
+                this.value = value.charAt(0);
+                break;
+            case 0:
+                throw new IllegalArgumentException("Empty punctuation mark");
+            default:
+                // Punctuation mark's length is >1 so it'll be ignored
+                throw new IllegalArgumentException("Incorrect punctuation mark");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getValue();
+    }
 }
 
 /**
@@ -31,6 +129,27 @@ class PunctuationMark extends SentenceElement {
  * is that the Word contains String value, PM contains char value.
  */
 class SentenceElement {
+    private String value;
+
+    public SentenceElement() {
+        this.value = "";
+    }
+    public SentenceElement(String value) {
+        this.value = value;
+    }
+    public SentenceElement(Word word) {
+        setValue(word.getValue());
+    }
+    public SentenceElement(PunctuationMark punctuationMark) {
+        setValue(punctuationMark.getValue());
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
 
 /**
@@ -38,10 +157,50 @@ class SentenceElement {
  * words and/or punctuation marks.
  */
 class Sentence {
+    protected ArrayList<SentenceElement> value;
+
+    public Sentence() {
+        this.value = new ArrayList<>(1);
+        value.add(new Word());
+        value.add(new PunctuationMark());
+        value.add(new Word());
+    }
+    public Sentence(ArrayList<SentenceElement> sentence) {
+        this.value = sentence;
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        for (SentenceElement se : value) {
+            result += se;
+        }
+        return String.format("%s", result);
+    }
 }
 
 /**
  * Class implements the text - a sequence of sentences and/or punctuation marks.
  */
 class Text {
+    protected ArrayList<Sentence> value;
+
+    public Text() {
+        this.value = new ArrayList<>(2);
+        value.add(new Sentence());
+        value.add(new Sentence());
+    }
+
+    public ArrayList<Sentence> getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        for (Sentence sentence : value) {
+            result += sentence + "\n";
+        }
+        return String.format("%s", result);
+    }
 }
