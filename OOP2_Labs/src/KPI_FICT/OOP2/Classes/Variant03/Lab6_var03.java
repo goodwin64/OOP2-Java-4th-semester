87d90570e3092c2fe51add9167899470ad66d10e
Max Donchenko <wereya2@gmail.com>
2016-03-20 18:58:03 +0200

    Lab 6-03: added standard Airplane methods
    Added constructors, getters, setters and somewhere documentation.

--- OOP2_Labs/src/KPI_FICT/OOP2/Classes/Variant03/Lab6_var03.java
+++ OOP2_Labs/src/KPI_FICT/OOP2/Classes/Variant03/Lab6_var03.java
@@ -1,21 +1,38 @@
 package KPI_FICT.OOP2.Classes.Variant03;
 
+import java.util.Locale;
+
 /**
  * Created by Max Donchenko (https://github.com/goodwin64) on 20.03.2016.
  */
 public class Lab6_var03 {
     public static void main(String[] args) {
-        Airplane ap1 = new Airplane();
+        Airplane ap1 = new Airplane("Il-2", 6160, 89.2);
+
         Airplane ap2 = new Airplane();
-        ap2.fuelConsumption = 10.5;
-        ap2.mass = 992.4;
-        ap2.model = "Cessna 172";
+        ap2.setFuelConsumption(10.5);
+
+        try {
+            ap2.setMass(-120); // Exception
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
+        ap2.setMass(992.4);
+        ap2.addMass(7.6);
+
+        ap2.setModel("Cessna 172");
 
         System.out.println(ap2.repairEngine(-5.2)); // false
         System.out.println(ap2.repairEngine(5.2)); // true
-        System.out.println(ap2.fuelConsumption); // 5.3
-        System.out.println(ap1.ID); // 1
-        System.out.println(ap2.ID); // 2
+
+        System.out.println(ap1.getFuelConsumption()); // 89.2
+        System.out.println(ap2.getFuelConsumption()); // 5.3
+
+        System.out.println(ap1.getMass()); // 6160
+        System.out.println(ap2.getMass()); // 1000
+
+        System.out.println(ap1.getID()); // 1
+        System.out.println(ap2.getID()); // 2
     }
 }
 
@@ -23,19 +40,93 @@ public class Lab6_var03 {
  * Parent class for all airplanes
  */
 class Airplane {
-    public int flightDistance;
-    public int ID;
-    public static int maxID = 0;
-    public double fuelConsumption;
-    public double mass; // kg
-    public String model;
+    private int flightDistance;
+    private int ID;
+    private static int maxID = 0;
+    private double fuelConsumption;
+    private double mass; // kg
+    private String model;
 
     public Airplane() {
         this.flightDistance = 0;
         this.ID = maxID + 1;
-        this.maxID++;
+        maxID++;
     }
 
+    public Airplane(String model, double mass,
+                    double fuelConsumption) throws IllegalArgumentException {
+        this.flightDistance = 0;
+        this.ID = maxID + 1;
+        maxID++;
+        this.fuelConsumption = fuelConsumption;
+        setMass(mass);
+        if (model.length() < 50) {
+            this.model = model;
+        }
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+
+    public double getMass() {
+        return mass;
+    }
+
+    public void setMass(double mass) {
+        if (mass > 70.0) {
+            this.mass = mass;
+        } else {
+            String message = String.format(Locale.ENGLISH, "Incorrect mass (%.2f kg)", mass);
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public void addMass(double mass) {
+        if (mass > 0) {
+            this.mass += mass;
+        }
+    }
+
+
+    public int getFlightDistance() {
+        return flightDistance;
+    }
+
+    public void setFlightDistance(int flightDistance) {
+        this.flightDistance = flightDistance;
+    }
+
+
+    public int getID() {
+        return ID;
+    }
+
+    public static int getMaxID() {
+        return maxID;
+    }
+
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+
+    /**
+     * Engine repair will reduce fuel consumption.
+     *
+     * @return true     if repair was successful
+     * @return false    otherwise
+     */
     public boolean repairEngine(double fuelConsumptionReduction) {
         if (fuelConsumptionReduction > 0
                 && this.fuelConsumption > fuelConsumptionReduction) {
