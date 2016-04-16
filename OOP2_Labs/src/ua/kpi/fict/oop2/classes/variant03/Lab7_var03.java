f80b3b6e2a773da722984aea2b25f1132eca486a
Max Donchenko <wereya2@gmail.com>
2016-04-16 16:53:39 +0300

    Lab 7-03: all classes in one .java file
    Class DoublyLinkedList moved into class Lab7_var03, now it's public
    static.

--- OOP2_Labs/src/ua/kpi/fict/oop2/classes/variant03/Lab7_var03.java
+++ OOP2_Labs/src/ua/kpi/fict/oop2/classes/variant03/Lab7_var03.java
@@ -219,152 +219,151 @@ public class Lab7_var03 {
             return result;
         }
     }
-}
 
-class DoublyLinkedList<E> implements Iterable<E> {
-    private Node head;
-    private Node tail;
-    private int size;
+    public static class DoublyLinkedList<E> implements Iterable<E> {
+        private Node head;
+        private Node tail;
+        private int size;
 
-    public DoublyLinkedList() {
-        size = 0;
-    }
+        public DoublyLinkedList() {
+            size = 0;
+        }
 
-    @Override
-    public Iterator<E> iterator() {
-        return new Iterator<E>() {
-            private Node temp = new Node(null, head, null);
+        @Override
+        public Iterator<E> iterator() {
+            return new Iterator<E>() {
+                private Node temp = new Node(null, head, null);
 
-            @Override
-            public boolean hasNext() {
-                return temp.next != null;
+                @Override
+                public boolean hasNext() {
+                    return temp.next != null;
+                }
+
+                @Override
+                public E next() {
+                    temp = temp.next;
+                    return temp.element;
+                }
+            };
+        }
+
+        /**
+         * This class keeps track of each element information.
+         */
+        private class Node {
+            E element;
+            Node next;
+            Node prev;
+
+            public Node(E element, Node next, Node prev) {
+                this.element = element;
+                this.next = next;
+                this.prev = prev;
             }
 
-            @Override
-            public E next() {
-                temp = temp.next;
-                return temp.element;
+            public boolean equals(Node other) {
+                return this.element.equals(other.element);
             }
-        };
-    }
-
-    /**
-     * This class keeps track of each element information.
-     */
-    private class Node {
-        E element;
-        Node next;
-        Node prev;
-
-        public Node(E element, Node next, Node prev) {
-            this.element = element;
-            this.next = next;
-            this.prev = prev;
+        }
+        /**
+         * Returns the size of the linked list.
+         */
+        public int getSize() {
+            return size;
         }
 
-        public boolean equals(Node other) {
-            return this.element.equals(other.element);
+        /**
+         * Returns whether the list is empty or not.
+         */
+        public boolean isEmpty() {
+            return size == 0;
         }
-    }
-    /**
-     * Returns the size of the linked list.
-     */
-    public int getSize() {
-        return size;
-    }
 
-    /**
-     * Returns whether the list is empty or not.
-     */
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    /**
-     * Adds element at the start.
-     * @param element       element to add
-     */
-    public void addFirst(E element) {
-        Node tmp = new Node(element, head, null);
-        if (head != null) {
-            head.prev = tmp;
-        }
-        head = tmp;
-        if (tail == null) {
-            tail = tmp;
-        }
-        size++;
-    }
-
-    /**
-     * Adds element at the end.
-     * @param element       element to add
-     */
-    public void addLast(E element) {
-        Node tmp = new Node(element, null, tail);
-        if (tail != null) {
-            tail.next = tmp;
-        }
-        tail = tmp;
-        if (head == null) {
+        /**
+         * Adds element at the start.
+         * @param element       element to add
+         */
+        public void addFirst(E element) {
+            Node tmp = new Node(element, head, null);
+            if (head != null) {
+                head.prev = tmp;
+            }
             head = tmp;
+            if (tail == null) {
+                tail = tmp;
+            }
+            size++;
         }
-        size++;
-    }
 
-    public boolean remove(E e) {
-        if (head.element.equals(e)) {
-            head.next.prev = null;
-            head = head.next;
-            size--;
-            return true;
-        } else if (tail.element.equals(e)) {
-            tail.prev.next = null;
-            tail = tail.prev;
-            size--;
-            return true;
+        /**
+         * Adds element at the end.
+         * @param element       element to add
+         */
+        public void addLast(E element) {
+            Node tmp = new Node(element, null, tail);
+            if (tail != null) {
+                tail.next = tmp;
+            }
+            tail = tmp;
+            if (head == null) {
+                head = tmp;
+            }
+            size++;
         }
-        Node temp = this.head.next;
-        while (temp != null) {
-            if (temp.element.equals(e)) {
-                temp.prev.next = temp.next;
-                temp.next.prev = temp.prev;
+
+        public boolean remove(E e) {
+            if (head.element.equals(e)) {
+                head.next.prev = null;
+                head = head.next;
+                size--;
+                return true;
+            } else if (tail.element.equals(e)) {
+                tail.prev.next = null;
+                tail = tail.prev;
                 size--;
                 return true;
             }
-            temp = temp.next;
-        }
-        return false;
-    }
-
-    /**
-     * Returns whether the list contains the element.
-     * @param element      a sought-for element
-     */
-    public boolean contains(E element) {
-        Node temp = this.head;
-        while (temp != null) {
-            if (temp.element.equals(element)) {
-                return true;
+            Node temp = this.head.next;
+            while (temp != null) {
+                if (temp.element.equals(e)) {
+                    temp.prev.next = temp.next;
+                    temp.next.prev = temp.prev;
+                    size--;
+                    return true;
+                }
+                temp = temp.next;
             }
-            temp = temp.next;
+            return false;
         }
-        return false;
-    }
 
-    @Override
-    public String toString() {
-        String result = "";
-        Node temp = this.head;
-        if (temp != null) {
-            result += temp.element;
-            temp = temp.next;
+        /**
+         * Returns whether the list contains the element.
+         * @param element      a sought-for element
+         */
+        public boolean contains(E element) {
+            Node temp = this.head;
+            while (temp != null) {
+                if (temp.element.equals(element)) {
+                    return true;
+                }
+                temp = temp.next;
+            }
+            return false;
         }
-        while (temp != null) {
-            result += " -> " + temp.element;
-            temp = temp.next;
+
+        @Override
+        public String toString() {
+            String result = "";
+            Node temp = this.head;
+            if (temp != null) {
+                result += temp.element;
+                temp = temp.next;
+            }
+            while (temp != null) {
+                result += " -> " + temp.element;
+                temp = temp.next;
+            }
+            return result;
         }
-        return result;
     }
 }
-
